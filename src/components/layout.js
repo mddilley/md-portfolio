452515d4c8a64843d5bfd978f3f947d377997c80
Michael Dilley <mike.dilley@gmail.com>
2020-06-01 00:22:24 -0500

    Add devicon CDN link to Helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,6 +29,10 @@ const Layout = ({ children }) => (
           ]}
         >
           <html lang="en" />
+          <link
+            rel="stylesheet"
+            href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css"
+          />
         </Helmet>
         <div id="wrapper">{children}</div>
       </>
